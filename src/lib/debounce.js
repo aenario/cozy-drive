5a7cd68ded6833b2d030d5ece6f1feb76a5edb70
Ben <mail1@sonadresse.com>
2017-07-02 23:24:36 +0200

    with optimized fuzzy words search and init with pouchdb db

---
+++ src/lib/debounce.js
@@ -0,0 +1,18 @@
+// usage :
+// func = function(){console.log('done 2s later')}
+// debounced_func = debounce(func, 2000)
+
+module.exports = function debounce(func, wait, immediate) {
+	var timeout
+	return function() {
+		var context = this, args = arguments
+		var later = function() {
+			timeout = null
+			if (!immediate) func.apply(context, args)
+		}
+		var callNow = immediate && !timeout
+		clearTimeout(timeout)
+		timeout = setTimeout(later, wait)
+		if (callNow) func.apply(context, args)
+	}
+}
